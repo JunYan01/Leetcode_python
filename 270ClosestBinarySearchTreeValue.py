9f4ee4bda56423698691742161686a01a2e60ce1
JunYan <jyan9@ncsu.edu>
2020-08-08 09:58:57 -0400

    Create 270ClosestBinarySearchTreeValue.py

---
+++ 270ClosestBinarySearchTreeValue.py
@@ -0,0 +1,32 @@
+# 270. Closest Binary Search Tree Value
+
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def closestValue(self, root: TreeNode, target: float) -> int:
+        def closestValueHelper(node,target) -> int:
+            if not node:
+                return float('inf')
+            minValue = node.val
+            mindis = abs(minValue - target)
+
+            a = closestValueHelper(node.left,target) 
+            disa = abs(a - target)
+            
+            if disa < mindis:
+                minValue, mindis = a, mindis
+                
+            a = closestValueHelper(node.right,target) 
+            disa = abs(a - target)
+            
+            if disa < mindis:
+                minValue, mindis = a, mindis
+            
+            return minValue
+        
+        return closestValueHelper(root,target)
