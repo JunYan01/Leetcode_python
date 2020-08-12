3f7e11dcdcd589abddc26842364910519a2763cc
JunYan <jyan9@ncsu.edu>
2020-08-12 18:54:47 -0400

    Update 119PascalsTriangleII.py

--- 119PascalsTriangleII.py
+++ 119PascalsTriangleII.py
@@ -3,20 +3,55 @@
 
 class Solution:
     def getRow(self, rowIndex: int) -> List[int]:
-        if rowIndex == 0:
-            return [1]
-        a = [1] * (rowIndex+1)
+        # a = [1] * (rowIndex+1)        
+        # if rowIndex <= 1:
+        #     return a
+        # for k in range(2,rowIndex+1):
+        #     mid = k//2+k%2
+        #     for i in range(mid,0,-1):
+        #         a[i] = a[i-1]+a[i]
+        #     a[k-mid+1:k] = a[mid-1:0:-1]
+        #     # for i in range(mid+1,k):
+        #     #     a[i] = a[k-i]
+        # return a
+
+
+        a = [1] * (rowIndex+1)        
+        if rowIndex <= 1:
+            return a
         for k in range(2,rowIndex+1):
-            a[0] = 1
-            a[k] = 1
-            for i in range(k//2+k%2,0,-1):
+            mid = k//2+k%2
+            for i in range(k-1,k-mid-1,-1):
                 a[i] = a[i-1]+a[i]
-            # print(k,k//2+k%2,a[0:k//2+k%2+1])
-            for i in range(k//2+k%2+1,k):
-                a[i] = a[k-i]
-            # print(a[0:k+1])
-            # print('adfasdfa')
+            # print(a[k-mid:k])
+            a[1:mid] =  a[k-1:k-mid:-1]
+            # print(a)
+        return a
+        
+#         a = [1] * (rowIndex+1)
+#         if rowIndex <= 1:
+#             return a
+        
+#         def getRowhelper(a,k):
+#             if k == rowIndex+1:
+#                 return a
+#             computation = []
+#             for i in range(1,k):
+#                 computation.append(a[i-1]+a[i])
+#             a[1:k] = computation
+#             print(computation)
+            
+#             getRowhelper(a,k+1)
+#             return a
                 
         
-        return a
+#         return getRowhelper(a,2)
+
+# 
+        # Other guys solution        
+        # row = [1]
+        # for _ in range(rowIndex):
+        #     row = [x + y for x, y in zip([0]+row, row+[0])]
+        # return row
+        
         
