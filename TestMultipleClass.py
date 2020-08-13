1335cd31aef8d32519e510404224af658157ca25
JunYan <jyan9@ncsu.edu>
2020-08-12 20:42:29 -0400

    Update TestMultipleClass.py

--- TestMultipleClass.py
+++ TestMultipleClass.py
@@ -20,38 +20,81 @@
 # b = B(1000)
 # print(b)
 
-class C:
+# class C:
 
-    class A:
-        def __init__(self,userID):
-            self.id = userID
+#     class A:
+#         def __init__(self,userID):
+#             self.id = userID
     
-        def __str__(self):
-            return str(self.id)
+#         def __str__(self):
+#             return str(self.id)
     
-    class B:
-        def __init__(self,userID):
-            self.aaa = self.A(userID//1000)
+#     class B:
+#         def __init__(self,userID):
+#             self.aaa = C.A(userID//1000)
     
-        def __str__(self):
-            return str(self.aaa)
+#         def __str__(self):
+#             return str(self.aaa)
 
-    def __init__(self,user1,user2):
-        self.aaa = self.A(user1)
-        self.bbb = self.B(user2)
+#     def __init__(self,user1,user2):
+#         self.aaa = self.A(user1)
+#         self.bbb = self.B(user2)
 
-    def printa(self):
-        print(self.aaa)
+#     def printa(self):
+#         print(self.aaa)
 
-    def printb(self):
-        print(self.bbb)
+#     def printb(self):
+#         print(self.bbb)
 
 
 
-a = C(200,1000)
-a.printa()
+# a = C(200,1000)
+# a.printa()
 
-a.printb()
+# a.printb()
+
+# from queue import PriorityQueue
+
+# customers = PriorityQueue() #we initialise the PQ class instead of using a function to operate upon a list. 
+# # customers.put((2, "Harry"))
+# # customers.put((3, "Charles"))
+# # customers.put((1, "Riya"))
+# # customers.put((4, "Stacy"))
+# customers.put(["Harry", 2])
+# customers.put(["Charles", 3])
+# customers.put(["Riya", 1])
+# customers.put(["Stacy", 4])
+# while not customers.empty():
+#      print(customers.get())
+#      # print(customers.get_nowait())
+
+try:
+    import Queue as Q  # ver. < 3.0
+except ImportError:
+    import queue as Q
+
+class Skill(object):
+    def __init__(self, priority, description):
+        self.priority = priority
+        self.description = description
+        print('New Level:', description)
+        return
+    # def __cmp__(self, other):
+    #     return cmp(self.priority, other.priority)
+    def __lt__(self, other):
+        return self.priority < other.priority
+
+
+q = Q.PriorityQueue()
+
+q.put(Skill(5, 'Proficient'))
+q.put(Skill(10, 'Expert'))
+q.put(Skill(1, 'Novice'))
+
+while not q.empty():
+    next_level = q.get()
+    print('Processing level:', next_level.description)
+
 
 
         
