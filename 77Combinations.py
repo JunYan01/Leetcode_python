2221de9f4da1a394b4e09f8c399c59c49bbfd220
JunYan <jyan9@ncsu.edu>
2020-08-13 17:06:13 -0400

    Update 77Combinations.py

--- 77Combinations.py
+++ 77Combinations.py
@@ -3,45 +3,55 @@
 
 class Solution:
     def combine(self, n: int, k: int) -> List[List[int]]:
-        track = [] if k <= n//2 else [x for x in range(1,n+1)]
-        res = [] 
+#         track = [] if k <= n//2 else [x for x in range(1,n+1)]
+#         res = [] 
 
-        def backtrack(n,start,track,res):
-            if track not in res and len(track) == k:
-                # print(track[:])
-                res.insert(0,track[:])
-                return
+#         def backtrack(n,start,track,res):
+#             if track not in res and len(track) == k:
+#                 # print(track[:])
+#                 res.insert(0,track[:])
+#                 return
             
             
-            # res.append(track)
-            # print(res,track)
-            for i in range(start,n):
-                track.insert(0,i+1)
-                backtrack(n,i+1,track,res)
-                track.pop(0)
-            return 
+#             # res.append(track)
+#             # print(res,track)
+#             for i in range(start,n):
+#                 track.insert(0,i+1)
+#                 backtrack(n,i+1,track,res)
+#                 track.pop(0)
+#             return 
         
-        def backtrack2(n,start,track,res):
-            if track not in res and len(track) == k:
-                # print(track[:])
-                res.insert(0,track[:])
-                return
+#         def backtrack2(n,start,track,res):
+#             if track not in res and len(track) == k:
+#                 # print(track[:])
+#                 res.insert(0,track[:])
+#                 return
             
             
-            # res.append(track)
-            # print(res,track)
-            for i in range(0,start+1):
-                # print(i,start)
-                a = track.pop(i)
-                backtrack2(n,i-1,track,res)
-                track.insert(i,a)
-            return 
+#             # res.append(track)
+#             # print(res,track)
+#             for i in range(0,start+1):
+#                 # print(i,start)
+#                 a = track.pop(i)
+#                 backtrack2(n,i-1,track,res)
+#                 track.insert(i,a)
+#             return 
         
-        if k <= n//2:
-            # print('adfadfa')
-            backtrack(n,0,track,res)
-        else:
-            # print('22222adfadfa')
-            backtrack2(n,n-1,track,res)
-        # print(len(res))
+#         if k <= n//2:
+#             # print('adfadfa')
+#             backtrack(n,0,track,res)
+#         else:
+#             # print('22222adfadfa')
+#             backtrack2(n,n-1,track,res)
+#         # print(len(res))
+#         return res
+        res = []
+        a = []
+        for bitmask in range(1<<n):
+            if bin(bitmask).count('1') == k:
+                # print(bin(bitmask))
+                a = [i+1 for i in range(n) if bitmask & 1<<i]
+                # print(a)
+                res.append(a)
+                
         return res
