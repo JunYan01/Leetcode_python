41d9eafaeeaf09cca7690b297eb3bf3342978a31
JunYan <jyan9@ncsu.edu>
2020-08-15 02:25:54 -0400

    Update 869ReorderedPowerof2.py

--- 869ReorderedPowerof2.py
+++ 869ReorderedPowerof2.py
@@ -9,9 +9,13 @@ class Solution:
 #         N =  ''.join(sorted(str(N))) 
 #         # print(N)
 #         return N in x
-        x = [int(''.join(sorted(str(2**i),reverse = True)) ) for i in range(31)]
-        # print(x)
-        # print(''.join(sorted(str(N))) )
-        N =  int(''.join(sorted(str(N),reverse = True)) )
-        # print(N)
-        return N in x
+
+
+        # x = [int(''.join(sorted(str(2**i),reverse = True)) ) for i in range(31)]
+        # # print(x)
+        # # print(''.join(sorted(str(N))) )
+        # N =  int(''.join(sorted(str(N),reverse = True)) )
+        # # print(N)
+        # return N in x
+
+        return sorted(str(N)) in [sorted(str(1 << i)) for i in range(30)]
