096ca474bb168f71083f4ff255297fb6d57f9fa9
JunYan <jyan9@ncsu.edu>
2020-08-23 14:17:29 -0400

    Update 1032StreamofCharacters.py

--- 1032StreamofCharacters.py
+++ 1032StreamofCharacters.py
@@ -7,7 +7,7 @@ class StreamChecker:
     def __init__(self, words: List[str]):
         self.trie = {}
         self.stream = deque([])
-
+        # self.stream = []
         for word in set(words):
             node = self.trie       
             for ch in word[::-1]:
@@ -19,14 +19,18 @@ class StreamChecker:
         
     def query(self, letter: str) -> bool:
         self.stream.appendleft(letter)
-        
+        # self.stream.insert(0,letter)
         node = self.trie
         for ch in self.stream:
+            # 见好就收, self.trie（node）里已经有符合要求的
             if '$' in node:
                 return True
+            # 如果不match, 之前的必定也没有符合要求的
             if not ch in node:
                 return False
+            # 如果match此前的，必须继续走完检测
             node = node[ch]
+        # 如果self.stream走完，self.trie(node) 里还没有走完，就不对了
         return '$' in node
 
 #     def __init__(self, words: List[str]):
