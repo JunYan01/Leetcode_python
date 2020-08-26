cc6b60ccecaeebb96a08655bcaba5a936a1da920
JunYan <jyan9@ncsu.edu>
2020-08-26 09:05:38 -0400

    Update 412FizzBuzz.py

--- 412FizzBuzz.py
+++ 412FizzBuzz.py
@@ -13,5 +13,19 @@ class Solution:
             else:
                 a.append(str(i))
         return a
+
+        # res = [0] *n
+        # for i in range(1,n+1):
+        #     if i % 15 == 0:
+        #         res[i-1] = "FizzBuzz"
+        #     elif i % 3 == 0:
+        #         res[i-1] = "Fizz"
+        #     elif i % 5 == 0:
+        #         res[i-1] = "Buzz"
+        #     else:
+        #         res[i-1] = str(i)
+        # return res
+        
+        # return ["FizzBuzz" if i % 15 == 0 else "Fizz" if i % 3 == 0 else "Buzz" if i % 5 == 0 else str(i) for i in range(1,n+1)]
                 
             
