71696f190061f45c3a5ee8482ccb33e260a537fc
JunYan <jyan9@ncsu.edu>
2020-08-30 00:32:00 -0400

    Create 969Pancakesorting.py

---
+++ 969Pancakesorting.py
@@ -0,0 +1,15 @@
+# 969. Pancake Sorting
+
+class Solution:
+    def pancakeSort(self, A: List[int]) -> List[int]:
+        res, n = [], len(A)
+        for i in range(n,0,-1):
+            pos = A.index(i)
+            if pos == i - 1:
+                continue
+            if pos != 0:
+                A[:pos+1] = A[:pos+1][::-1]
+                res.append(pos+1)
+            A[:i] =  A[:i][::-1]
+            res.append(i)
+        return res
