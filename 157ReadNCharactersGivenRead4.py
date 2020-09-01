ed081f03d77f4b4119237d4693e2a6a77998c495
JunYan <jyan9@ncsu.edu>
2020-09-01 10:59:55 -0400

    Create 157ReadNCharactersGivenRead4.py

---
+++ 157ReadNCharactersGivenRead4.py
@@ -0,0 +1,37 @@
+# 157. Read N Characters Given Read4
+
+"""
+The read4 API is already defined for you.
+
+    @param buf4, a list of characters
+    @return an integer
+    def read4(buf4):
+
+# Below is an example of how the read4 API can be called.
+file = File("abcdefghijk") # File is "abcdefghijk", initially file pointer (fp) points to 'a'
+buf4 = [' '] * 4 # Create buffer with enough space to store characters
+read4(buf4) # read4 returns 4. Now buf = ['a','b','c','d'], fp points to 'e'
+read4(buf4) # read4 returns 4. Now buf = ['e','f','g','h'], fp points to 'i'
+read4(buf4) # read4 returns 3. Now buf = ['i','j','k',...], fp points to end of file
+"""
+
+class Solution:
+    def read(self, buf, n):
+        """
+        :type buf: Destination buffer (List[str])
+        :type n: Number of characters to read (int)
+        :rtype: The number of actual characters read (int)
+        """
+        read_bytes = 0
+        buffer = [''] * 4
+        for i in range(n // 4 + 1):
+            size = read4(buffer)
+            if size:
+                size = min(size, n-read_bytes)
+                buf[read_bytes:read_bytes+size] = buffer[:size]
+                read_bytes += size
+            else:
+                break
+        # print(buf)
+        return read_bytes
+        
