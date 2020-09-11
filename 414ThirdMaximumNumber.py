e36a1523b52f3d4a827525e9136f79611c0fc93e
JunYan <jyan9@ncsu.edu>
2020-09-10 20:46:57 -0400

    Create 414ThirdMaximumNumber.py

---
+++ 414ThirdMaximumNumber.py
@@ -0,0 +1,14 @@
+# 414. Third Maximum Number
+
+class Solution:
+    def thirdMax(self, nums: List[int]) -> int:
+        maximums = set()
+        for num in nums:
+            maximums.add(num)
+            if len(maximums) > 3:
+                maximums.remove(min(maximums))
+        if len(maximums) == 3:
+            return min(maximums)
+        return max(maximums)
+        
+        
