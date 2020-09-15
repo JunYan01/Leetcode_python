840ff5a4bd7acc7911478443df5a2da331874e17
JunYan <jyan9@ncsu.edu>
2020-09-15 13:11:45 -0400

    Update 198HouseRobber.py

--- 198HouseRobber.py
+++ 198HouseRobber.py
@@ -40,6 +40,37 @@ class Solution:
         
         # return dp
 
+        #         n = len(nums)
+#         if n == 0: return 0
+#         dp = [[0] * n for _ in range(n)]
+        
+#         for i in range(n-1,-1,-1):
+#             dp[i][i] = nums[i]
+#             if i + 1 < n:
+#                 dp[i][i+1] = max(nums[i],nums[i+1])
+#             for j in range(i+2,n):
+#                 dp[i][j] = max(dp[i][j-2] + nums[j],dp[i][j-1])
+                
+#         return dp[0][n-1]
+
+
+        # n = len(nums)
+        # if n == 0: return 0
+        # if n == 1: return nums[0]
+        # dp = [0]*n
+        # dp[0] = nums[0]
+        
+        # dp[1] = max(nums[0],nums[1])
+        # for i in range(2,n):
+        #     dp[i] = max(dp[i-2]+nums[i],dp[i-1])
+        
+        # return dp[n-1]
+
+        dp0 = dp1 = 0
+        for i in range(len(nums)):
+            dp0, dp1 = dp1, max(dp0+nums[i],dp1)
+        return dp1
+
         
 
 a = Solution()
