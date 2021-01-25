1ebc0813a258b12d0e0f879c8d4560d503cc6733
JunYan <jyan9@ncsu.edu>
2021-01-25 00:20:46 -0500

    Create 364NestedListWeightSum.py

---
+++ 364NestedListWeightSum.py
@@ -0,0 +1,58 @@
+364 Nested List Weight Sum
+
+# """
+# This is the interface that allows for creating nested lists.
+# You should not implement it, or speculate about its implementation
+# """
+#class NestedInteger:
+#    def __init__(self, value=None):
+#        """
+#        If value is not specified, initializes an empty list.
+#        Otherwise initializes a single integer equal to value.
+#        """
+#
+#    def isInteger(self):
+#        """
+#        @return True if this NestedInteger holds a single integer, rather than a nested list.
+#        :rtype bool
+#        """
+#
+#    def add(self, elem):
+#        """
+#        Set this NestedInteger to hold a nested list and adds a nested integer elem to it.
+#        :rtype void
+#        """
+#
+#    def setInteger(self, value):
+#        """
+#        Set this NestedInteger to hold a single integer equal to value.
+#        :rtype void
+#        """
+#
+#    def getInteger(self):
+#        """
+#        @return the single integer that this NestedInteger holds, if it holds a single integer
+#        Return None if this NestedInteger holds a nested list
+#        :rtype int
+#        """
+#
+#    def getList(self):
+#        """
+#        @return the nested list that this NestedInteger holds, if it holds a nested list
+#        Return None if this NestedInteger holds a single integer
+#        :rtype List[NestedInteger]
+#        """
+
+class Solution:
+    def depthSum(self, nestedList: List[NestedInteger]) -> int:
+        def depthSumHelper(nestedList: List[NestedInteger], depth = 1) -> int:
+            count = 0 
+            for elem in nestedList:
+                if elem.isInteger():
+                    count += elem.getInteger()*depth
+                else:
+                    count += depthSumHelper(elem.getList(), depth + 1)
+            return count
+        
+        return depthSumHelper(nestedList)
+        
