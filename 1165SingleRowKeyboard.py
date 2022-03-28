8764404d1ef758aa408df577fe1a20467c7b6dbb
Jun <5742401+JunYan01@users.noreply.github.com>
2022-03-28 17:17:08 +0800

    Update 1165SingleRowKeyboard.py

--- 1165SingleRowKeyboard.py
+++ 1165SingleRowKeyboard.py
@@ -1,4 +1,4 @@
-1165  Single-Row Keyboard
+# 1165  Single-Row Keyboard
 
 class Solution:
     def calculateTime(self, keyboard: str, word: str) -> int:
@@ -13,3 +13,10 @@ class Solution:
             # print(val)
         return val
             
+
+if __name__ == '__main__':
+    solution = Solution()
+    print(solution.calculateTime('adfadfadf','dfa'))
+    import math
+
+    print(math.sin(30))
